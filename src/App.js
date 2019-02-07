a7ff092254b355978b55b4e4364b81f913808575
Alan19 <Alan19@users.noreply.github.com>
2019-02-06 19:11:47 -0500

    Start adding detailed view

--- src/App.js
+++ src/App.js
@@ -1,59 +1,36 @@
-import React, {Component} from 'react';
-import './App.css';
-import Header from './Header.js';
+import React, {Component} from "react";
+import "./App.css";
+import Header from "./Header.js";
 import Footer from "./Footer";
 import {cultureDescription, magicDescription, wayDescription} from "./store";
-import {Card, CardContent, Typography} from "@material-ui/core";
-import FallBack from "./resources/psychic-waves.svg";
-import Grid from '@material-ui/core/Grid';
-
-
-function generateInfo(currentView) {
-    return (<Grid container spacing={40}>
-        {currentView.map((skill) => (
-            <Grid item sm style={{flexGrow: 1, padding: 30}}>
-                <Card>
-                    <CardContent>
-                        <Typography variant={"title"} style={{margin: 10}}>
-                            <img src={skill.icon} height={24} style={{flex: 1}}
-                                 alt={FallBack}/> {skill.name}
-                        </Typography>
-                        <Typography variant={"body1"}
-                                    style={{margin: 10}}>{skill.description}</Typography></CardContent>
-                </Card>
-            </Grid>
-
-        ))}
-    </Grid>);
-}
+import {GenerateInfo} from "./SkillCard";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentView: magicDescription
+            currentView: magicDescription,
+            inDepth: false
         };
         this.changeView = this.changeView.bind(this);
     }
 
     changeView(viewName) {
-        console.log('Received ' + viewName);
-        if (viewName === 'magic') {
-            this.setState({currentView: magicDescription});
-        } else if (viewName === 'way') {
-            this.setState({currentView: wayDescription});
-        } else if (viewName === 'culture') {
-            this.setState({currentView: cultureDescription});
+        if (viewName === "magic" && this.state.currentView !== magicDescription) {
+            this.setState({currentView: magicDescription, inDepth: false});
+        } else if (viewName === "way" && this.state.currentView !== wayDescription) {
+            this.setState({currentView: wayDescription, inDepth: false});
+        } else if (viewName === "culture" && this.state.currentView !== cultureDescription) {
+            this.setState({currentView: cultureDescription, inDepth: false});
         }
     }
 
     render() {
-        console.log(magicDescription);
         return (
             <div className="App">
                 <Header onclick={this.changeView}/>
                 <div style={{padding: 20}}>
-                    {generateInfo(this.state.currentView)}
+                    <GenerateInfo currentView={this.state.currentView}/>
                 </div>
                 <Footer/>
             </div>
---
+++ src/SkillCard.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import {Button, Card, CardActions, CardContent, CardHeader, Typography} from "@material-ui/core";
+import Grid from '@material-ui/core/Grid'
+
+function SkillTitle(props) {
+    return (
+        <>
+            <img
+                src={props.skill.icon}
+                height={20}
+                alt={props.skill.name}
+            />{" "}
+            {props.skill.name}
+        </>
+    );
+}
+
+export class SkillCard extends Component {
+    render() {
+        return (
+            <Card>
+                <CardHeader title={<SkillTitle skill={this.props.skill}/>}/>
+                <Typography/>
+                <CardContent style={{height:200, textOverflow: 'ellipsis', overflow: 'hidden'}}>
+                    <Typography variant={"body1"}>{this.props.skill.description}</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        );
+    }
+
+}
+
+export class GenerateInfo extends React.Component {
+    render() {
+        return (
+            <Grid container justify="flex-start" alignItems={"stretch"}>
+                {this.props.currentView.map(skill => (
+                    <Grid key={skill.id} item sm={3} style={{padding: 30}}>
+                        <SkillCard skill={skill}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    }
+
+}
