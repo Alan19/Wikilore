3c57bd482c11d14da1a23969f8702950fc893796
Alan19 <Alan19@users.noreply.github.com>
2019-02-08 01:40:31 -0500

    Added copyright

--- src/Footer.js
+++ src/Footer.js
@@ -1,25 +1,37 @@
-import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import {BottomNavigation, BottomNavigationAction, Typography} from "@material-ui/core";
 import MagicIcon from "./icon-classes/magic-icon";
 import WayIcon from "./icon-classes/way-icon";
 import CultureIcon from "./icon-classes/culture-icon";
 import React from "react";
+import {copyright} from './store'
 
 export default getFooter;
+
+const bottomNav = <BottomNavigation
+    style={{ width: "100%" }}
+    showLabels
+    value={0}
+>
+  <BottomNavigationAction icon={<MagicIcon />} label="Magic" />
+  <BottomNavigationAction icon={<WayIcon />} label="Way" />
+  <BottomNavigationAction icon={<CultureIcon />} label="Culture" />
+</BottomNavigation>;
+
+const copyrightText = (<Typography variant={"caption"} style={{right:0}}>{copyright}</Typography>);
+
 
 function getFooter(props) {
   if (window.innerWidth <= 500) {
     return (
-      <BottomNavigation
-        style={{ position: "sticky", bottom: "0", width: "100%" }}
-        showLabels
-        value={0}
-      >
-        <BottomNavigationAction icon={<MagicIcon />} label="Magic" />
-        <BottomNavigationAction icon={<WayIcon />} label="Way" />
-        <BottomNavigationAction icon={<CultureIcon />} label="Culture" />
-      </BottomNavigation>
+        <span style={{ position: "sticky", bottom: "0"}}>
+          {bottomNav}
+          <div style={{position: 'relative', marginLeft: 10}}>{copyrightText}</div>
+        </span>
+
     );
   } else {
-    return null;
+    return <div style={{ position: "fixed", bottom: 0, right: 0, marginRight:10, marginBottom: 10}}>{copyrightText}</div>;
   }
+
 }
+
