ed4d1e5885d7970fe5de8b6f9bf13c0ba0f2a1ab
Alan19 <Alan19@users.noreply.github.com>
2019-03-07 11:22:32 -0500

    Expansion panels automatically expand on desktop for easy searching

--- src/components/InDepthSkillList.js
+++ src/components/InDepthSkillList.js
@@ -5,14 +5,14 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import Typography from "@material-ui/core/Typography";
 
 export class InDepthSkillList extends React.Component {
-  render() {
+    render() {
+      this.collapsed = [];
     let skillList = this.props.skillList;
-    console.log(this.props.skillList);
     let skillListComponents = [];
     for (let i = 0; i < skillList.length; i++) {
-      if (i < skillList.length - 1) {
+        this.collapsed[i] = !this.props.isDesktop;
         skillListComponents.push(
-          <ExpansionPanel>
+          <ExpansionPanel defaultExpanded={this.props.isDesktop}>
               <ExpansionPanelSummary><Typography>{skillList[i].name}</Typography></ExpansionPanelSummary>
               <ExpansionPanelDetails>
             <InDepthView
@@ -22,13 +22,7 @@ export class InDepthSkillList extends React.Component {
               </ExpansionPanelDetails>
           </ExpansionPanel>
         );
-      } else {
-        skillListComponents.push(
-          <ExpansionPanel>
-            <InDepthView skillObject={skillList[i]} />
-          </ExpansionPanel>
-        );
-      }
+
     }
     return skillListComponents;
   }
