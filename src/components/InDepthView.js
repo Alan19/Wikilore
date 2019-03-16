d905ebb304fc2006b4cc38e69a87664823ffb665
Alan19 <Alan19@users.noreply.github.com>
2019-03-16 16:24:51 -0400

    Set up HashLinks

--- src/components/InDepthView.js
+++ src/components/InDepthView.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import {Divider, Grow, Typography, withStyles} from "@material-ui/core";
+import { Divider, Grow, Typography, withStyles } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-
+import { HashLink as Link } from "react-router-hash-link";
 
 const styles = {
   anchorTarget: {
     // paddingTop: 70, marginTop: -70
   }
+};
+
+const scrollWithOffset = (el, offset) => {
+  const elementPosition = el.offsetTop - offset;
+  window.scroll({
+    top: elementPosition,
+    left: 0
+  });
 };
 
 /**
@@ -17,41 +25,48 @@ class InDepthView extends Component {
   render() {
     const skill = this.props.skillObject.detailedDescription;
     const icon = this.props.skillObject.icon;
-    const {classes} = this.props;
+    const { classes } = this.props;
     return (
       <React.Fragment>
         <Grow in={true}>
           <div
-          style={{
-            margin: "auto",
-          }}
+            style={{
+              margin: "auto"
+            }}
           >
-            <Grid wrap={"nowrap"} direction={!this.props.isDesktop ? "column-reverse" : "row"} container spacing={this.props.theme.spacing.unit * 3}>
-              <Grid item md={2}/>
+            <Grid
+              wrap={"nowrap"}
+              direction={!this.props.isDesktop ? "column-reverse" : "row"}
+              container
+              spacing={this.props.theme.spacing.unit * 3}
+            >
+              <Grid item md={2} />
               <Grid item md={6}>
-                <a id={skill.title.toLowerCase().replace(/\s/g, "")} />
                 <Typography
                   variant={"h3"}
                   style={{ overflow: "auto", overflowY: "hidden" }}
                   component={"span"}
-                  className={classes.anchorTarget}
+                  id={skill.title.toLowerCase().replace(/\s/g, "")}
                 >
                   {skill.title} <img src={icon} height={40} alt={skill.title} />
                 </Typography>
-                <Typography className={classes.anchorTarget} variant={"subtitle2"} paragraph={true}>
+                <Typography
+                  className={classes.anchorTarget}
+                  variant={"subtitle2"}
+                  paragraph={true}
+                >
                   {skill.blurb}
                 </Typography>
                 {skill.sections.map(section => (
                   <React.Fragment>
-                    <a id={section.title.toLowerCase().replace(/\s/g, "")} />
                     <Typography
-                        className={classes.anchorTarget}
+                      id={section.title.toLowerCase().replace(/\s/g, "")}
                       variant={"subtitle1"}
                     >
                       {section.title}
                     </Typography>
                     <Typography
-                        className={classes.anchorTarget}
+                      className={classes.anchorTarget}
                       variant={"body1"}
                       paragraph={true}
                       component={"span"}
@@ -61,21 +76,26 @@ class InDepthView extends Component {
                   </React.Fragment>
                 ))}
                 <Divider variant={"middle"} light />
-                <a id={skill.purchasableSkillType.toLowerCase().replace(/\s/g, "")} />
-                <Typography paragraph className={classes.anchorTarget} style={{ paddingTop: 16 }} variant={"h5"}>
+                <Typography
+                  paragraph
+                  id={skill.purchasableSkillType
+                    .toLowerCase()
+                    .replace(/\s/g, "")}
+                  style={{ paddingTop: 16 }}
+                  variant={"h5"}
+                >
                   {skill.purchasableSkillType}
                 </Typography>
                 {skill.effects.map(section => (
                   <React.Fragment>
-                    <a id={section.title.toLowerCase().replace(/\s/g, "")} />
                     <Typography
-                        className={classes.anchorTarget}
+                      id={section.title.toLowerCase().replace(/\s/g, "")}
                       variant={"subtitle1"}
                     >
                       {section.title}
                     </Typography>
                     <Typography
-                        className={classes.anchorTarget}
+                      className={classes.anchorTarget}
                       variant={"body1"}
                       paragraph={true}
                       component={"span"}
@@ -87,35 +107,44 @@ class InDepthView extends Component {
               </Grid>
 
               {/*Table of Contents*/}
-                <Grid item md={4}>
-                  <a href={"/#" + skill.title.toLowerCase().replace(/\s/g, "")}>
-                    <Typography variant={"overline"}>{skill.title}</Typography>
-                  </a>
-                  {skill.sections.map(section => (
-                    <a
-                      href={
-                        "/#" + section.title.toLowerCase().replace(/\s/g, "")
-                      }
-                    >
-                      <Typography variant={"subtitle1"}>
-                        {section.title}
-                      </Typography>
-                    </a>
-                  ))}
-                  <a href={"/#" + skill.purchasableSkillType.toLowerCase().replace(/\s/g, "")}>
-                    <Typography variant={"overline"}>{skill.purchasableSkillType}</Typography>
-                  </a>
-                  {skill.effects.map(section => (
-                      <a
-                          href={
-                            "/#" + section.title.toLowerCase().replace(/\s/g, "")
-                          }
-                      >
-                        <Typography variant={"subtitle1"}>
-                          {section.title}
-                        </Typography>
-                      </a>
-                  ))}
+              <Grid item md={4}>
+                <Link
+                  scroll={el => scrollWithOffset(el, 0)}
+                  to={"//#" + skill.title.toLowerCase().replace(/\s/g, "")}
+                >
+                  <Typography variant={"overline"}>{skill.title}</Typography>
+                </Link>
+                {skill.sections.map(section => (
+                  <Link
+                    scroll={el => scrollWithOffset(el, 0)}
+                    to={"//#" + section.title.toLowerCase().replace(/\s/g, "")}
+                  >
+                    <Typography variant={"subtitle1"}>
+                      {section.title}
+                    </Typography>
+                  </Link>
+                ))}
+                <Link
+                  scroll={el => scrollWithOffset(el, 0)}
+                  to={
+                    "//#" +
+                    skill.purchasableSkillType.toLowerCase().replace(/\s/g, "")
+                  }
+                >
+                  <Typography variant={"overline"}>
+                    {skill.purchasableSkillType}
+                  </Typography>
+                </Link>
+                {skill.effects.map(section => (
+                  <Link
+                    scroll={el => scrollWithOffset(el, 0)}
+                    to={"//#" + section.title.toLowerCase().replace(/\s/g, "")}
+                  >
+                    <Typography variant={"subtitle1"}>
+                      {section.title}
+                    </Typography>
+                  </Link>
+                ))}
               </Grid>
             </Grid>
           </div>
