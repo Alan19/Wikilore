00b1c2e92248ae980c874f921507209746dfdc87
Alan19 <Alan19@users.noreply.github.com>
2019-05-15 23:36:40 -0400

    Break up formatSkillText()

--- src/components/FormattedSkillText.js
+++ src/components/FormattedSkillText.js
@@ -2,43 +2,67 @@ import React from "react";
 import { Divider, Typography } from "@material-ui/core";
 
 export function generateFormattedSkillText(skill, icon) {
+  function generateTitle() {
+    return (
+      <>
+        <a id={skill.title.toLowerCase().replace(/\s/g, "")} />
+        <Typography
+          variant={"h3"}
+          style={{ overflow: "auto", overflowY: "hidden" }}
+          component={"span"}
+        >
+          {skill.title}{" "}
+          <img style={{ height: "1em" }} src={icon} alt={skill.title} />
+        </Typography>
+        <Typography variant={"subtitle2"} paragraph={true}>
+          {skill.blurb}
+        </Typography>
+      </>
+    );
+  }
+
+  function generateSections() {
+    return (
+      <>
+        {skill.sections.map(section => (
+          <React.Fragment>
+            <a id={section.title.toLowerCase().replace(/\s/g, "")} />
+            <Typography variant={"subtitle1"}>{section.title}</Typography>
+            <Typography variant={"body1"} paragraph={true} component={"span"}>
+              {section.text}
+            </Typography>
+          </React.Fragment>
+        ))}
+      </>
+    );
+  }
+
+  function generatePurchaseableSkills() {
+    return (
+      <>
+        <a id={skill.purchasableSkillType.toLowerCase().replace(/\s/g, "")} />
+        <Typography paragraph style={{ paddingTop: 16 }} variant={"h5"}>
+          {skill.purchasableSkillType}
+        </Typography>
+        {skill.effects.map(section => (
+          <React.Fragment>
+            <a id={section.title.toLowerCase().replace(/\s/g, "")} />
+            <Typography variant={"subtitle1"}>{section.title}</Typography>
+            <Typography variant={"body1"} paragraph={true} component={"span"}>
+              {section.text}
+            </Typography>
+          </React.Fragment>
+        ))}
+      </>
+    );
+  }
+
   return (
     <React.Fragment>
-      <a id={skill.title.toLowerCase().replace(/\s/g, "")} />
-      <Typography
-        variant={"h3"}
-        style={{ overflow: "auto", overflowY: "hidden" }}
-        component={"span"}
-      >
-        {skill.title}{" "}
-        <img style={{ height: "1em" }} src={icon} alt={skill.title} />
-      </Typography>
-      <Typography variant={"subtitle2"} paragraph={true}>
-        {skill.blurb}
-      </Typography>
-      {skill.sections.map(section => (
-        <React.Fragment>
-          <a id={section.title.toLowerCase().replace(/\s/g, "")} />
-          <Typography variant={"subtitle1"}>{section.title}</Typography>
-          <Typography variant={"body1"} paragraph={true} component={"span"}>
-            {section.text}
-          </Typography>
-        </React.Fragment>
-      ))}
+      {generateTitle()}
+      {generateSections()}
       <Divider variant={"middle"} light />
-      <a id={skill.purchasableSkillType.toLowerCase().replace(/\s/g, "")} />
-      <Typography paragraph style={{ paddingTop: 16 }} variant={"h5"}>
-        {skill.purchasableSkillType}
-      </Typography>
-      {skill.effects.map(section => (
-        <React.Fragment>
-          <a id={section.title.toLowerCase().replace(/\s/g, "")} />
-          <Typography variant={"subtitle1"}>{section.title}</Typography>
-          <Typography variant={"body1"} paragraph={true} component={"span"}>
-            {section.text}
-          </Typography>
-        </React.Fragment>
-      ))}
+      {generatePurchaseableSkills()}
     </React.Fragment>
   );
 }
