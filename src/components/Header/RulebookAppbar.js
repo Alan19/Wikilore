35ca3dd43b41aacbe14bd5bf60231f596a43f352
Alan19 <Alan19@users.noreply.github.com>
2019-12-24 16:09:10 -0500

    Set up drawer

---
+++ src/components/Header/RulebookAppbar.js
@@ -0,0 +1,40 @@
+import AppBar from "@material-ui/core/AppBar";
+import clsx from "clsx";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import * as PropTypes from "prop-types";
+import React from "react";
+
+export const RulebookAppbar = props => {
+  const { appBar, appBarShift } = props.classes;
+  return (
+    <AppBar
+      position="fixed"
+      className={clsx(appBar, {
+        [appBarShift]: props.open
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={props.onClick}
+          edge="start"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap>
+          Rulebook V3
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+RulebookAppbar.propTypes = {
+  classes: PropTypes.func,
+  open: PropTypes.bool,
+  onClick: PropTypes.func
+};
