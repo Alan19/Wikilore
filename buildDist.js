dbdbde9692982c405a540949554c6c4aed0651ed
Alan19 <Alan19@users.noreply.github.com>
2020-08-07 16:19:27 -0400

    Switch to zip-a-folder

--- buildDist.js
+++ buildDist.js
@@ -1,14 +1,7 @@
 let packageJSON = require("./package.json");
-let zipFolder = require("zip-folder");
-
+const { zip } = require('zip-a-folder');
 const exec = require("child_process").exec;
 console.log("Starting Build...");
-exec("npm run build", function() {
-  zipFolder("build", `./distributions/${packageJSON.name}${packageJSON.version}.zip`, function(err) {
-    if (err) {
-      console.log("oh no!", err);
-    } else {
-      console.log("Distribution Zipped");
-    }
-  });
+exec("npm run build", async function() {
+  await zip("build", `./distributions/${packageJSON.name}${packageJSON.version}.zip`).then(value => console.log(value));
 });
